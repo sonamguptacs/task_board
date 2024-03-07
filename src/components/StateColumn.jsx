787d19545f7f6dafa469d3fe32fbdd69521d3fb8
sonamguptacs <sonamcs3092@gmail.com>
2024-03-08 00:25:38 +0530

    added props for status

--- src/components/StateColumn.jsx
+++ src/components/StateColumn.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import './task.css'
 import Task from './Task'
 
-export const StateColumn = () => {
+export const StateColumn = (props) => {
   return (
     <div className="state_column">
+      <div className="caption" style={{ color: props.variant }}>
+        {props.type}
+      </div>
       <Task />
     </div>
   )
--- src/components/TaskBoard.jsx
+++ src/components/TaskBoard.jsx
@@ -4,8 +4,8 @@ import { StateColumn } from './StateColumn'
 export default function TaskBoard() {
   return (
     <div className={'task_board'}>
-      <StateColumn />
-      <StateColumn />
+      <StateColumn type={'New'} variant={'blue'} />
+      <StateColumn type={'In Progress'} variant={'orange'} />
     </div>
   )
 }
